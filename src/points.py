8b9c996d0c614819d02e8120f0d45e8c9c1da1a1
Paweł Janc <61934300+PawelJanc@users.noreply.github.com>
2020-06-16 13:57:52 +0200

    Update points.py

--- src/points.py
+++ src/points.py
@@ -37,8 +37,6 @@ class Point:
 
 if __name__ == '__main__':
     rp.init_node('points', log_level=rp.DEBUG)
-    #st = Point(1.2, 1.2, "start", (0.0, 1.0, 0.0))
-    #en = Point(2.8, 2.8, "end", (1.0, 0.0, 0.0))
     st = Point(10.0, 10.0, "start", (0.0, 1.0, 0.0))
     en = Point(15.0, 15.0, "end", (1.0, 0.0, 0.0))
     while not rp.is_shutdown():
